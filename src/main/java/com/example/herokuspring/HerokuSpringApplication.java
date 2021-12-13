3dd62705a8cd038656756e1e183fe423325255f0
atregon <adria.tregon.muniesa@gmail.com>
2021-12-13 13:19:36 +0100

    Initial commit

---
+++ src/main/java/com/example/herokuspring/HerokuSpringApplication.java
@@ -0,0 +1,23 @@
+package com.example.herokuspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class HerokuSpringApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HerokuSpringApplication.class, args);
+    }
+
+}
