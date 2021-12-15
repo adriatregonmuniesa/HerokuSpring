4996feec534739788e0fb110c525647e73dfb90e
atregon <adria.tregon.muniesa@gmail.com>
2021-12-15 12:16:43 +0100

    Prova RequestMapping Spring

--- src/main/java/com/example/herokuspring/HerokuSpringApplication.java
+++ src/main/java/com/example/herokuspring/HerokuSpringApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class HerokuSpringApplication {
 
-    @RequestMapping("/hola")
+    @RequestMapping("/")
     @ResponseBody
     String home(){
         return "Hello World!";
