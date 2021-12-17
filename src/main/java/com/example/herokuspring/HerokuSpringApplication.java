46ccef577750e3251cacde3e41b4bee3dd65709b
atregon <adria.tregon.muniesa@gmail.com>
2021-12-17 08:51:05 +0100

    Prova RequestMapping Spring4

--- src/main/java/com/example/herokuspring/HerokuSpringApplication.java
+++ src/main/java/com/example/herokuspring/HerokuSpringApplication.java
@@ -3,6 +3,7 @@ package com.example.herokuspring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,18 +11,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class HerokuSpringApplication {
 
-    int x = 2;
-    int[] array = new int[4];
+    //int x = 2;
+    //int[] array = new int[4];
 
-    public HerokuSpringApplication() {
+    //public HerokuSpringApplication() {
 
-        for(int i=0; i<array.length; i++){
-            array[i]=i+i;
-        }
-    }
+      //  for(int i=0; i<array.length; i++){
+        //    array[i]=i+i;
+        //}
+    //}
 
 
-    // @RequestMapping("/")
+    @RequestMapping("/")
     @ResponseBody
     String home(){
         return "Hello World!!";
@@ -32,13 +33,17 @@ public class HerokuSpringApplication {
 
     }
 
-    @RequestMapping("/")
-    void imprimirArray(){
-
-        for(int i=0; i<array.length; i++){
-            System.out.println("Valors: "+array[i]);
-        }
-
+    @GetMapping("/Main")
+    public String sayHello() {
+        return "hello Main";
     }
+
+    //void imprimirArray(){
+
+      //  for(int i=0; i<array.length; i++){
+        //    System.out.println("Valors: "+array[i]);
+        //}
+
+    //}
 
 }
